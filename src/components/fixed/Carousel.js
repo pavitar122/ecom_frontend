92254b9ffb152e2b2932aed8f1e11dc894ea30a8
pavitar122 <pavitar3335@gmail.com>
2024-07-05 20:52:45 +0530

    image

--- src/components/fixed/Carousel.js
+++ src/components/fixed/Carousel.js
@@ -1,7 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Headphone from "../images/slides/headphone.png"
-import Speaker from "../images/slides/speaker.png"
-import Earphone  from "../images/slides/earphone.png"
+
 
 
 function IndividualIntervalsExample() {
@@ -24,7 +23,7 @@ function IndividualIntervalsExample() {
               <h1 className='slide__content__heading'>Headphone</h1>
               <h2 className='slide__content__des'>Raining Offers For Hot Summer!</h2>
             </div>
-            <img className='slide__image' src={Speaker} alt='Headphone'></img>
+            <img className='slide__image' src={Headphone} alt='Headphone'></img>
           </div>
       </Carousel.Item>
 
